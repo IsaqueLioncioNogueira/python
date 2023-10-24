305d40230368c7e7a6cce98e47b9c4b946565fcb
Isaque Lioncio Nogueira <isaque.lioncio.nogueira@gmail.com>
2023-10-24 08:53:29 -0300

    made exercise 69

--- Exercícios/ex069.py
+++ Exercícios/ex069.py
@@ -3,3 +3,22 @@
 # A) quantas pessoas tem mais de 18 anos.
 # B) quantos homens foram cadastrados.
 # C) quantas mulheres tem menos de 20 anos.
+n1 = str
+n2 = 0
+n5 = 0
+n6 = 0
+while n1 != 'n' and n1 != 'N':
+    n3 = int(input('Digite o sexo de uma pessoa, 1 para homem e 2 para mulher: '))
+    while n3 != 1 and n3 != 2 :
+        n3 = int(input('Digite o sexo de uma pessoa, 1 para homem e 2 para mulher: '))
+    n4 = int(input('Digite a idade dessa pessoa: '))
+    n1 = input('Quer continuar? ')
+    while n1 != 'S'and n1 != 's' and n1 != 'n' and n1 != 'N':
+        n1 = input('Quer continuar? ')
+    if n1 != 'n'and n1 != 'N':
+        if n3 > 18 :
+            n2 += 1
+        if n4 == 1:
+            n5 += 1
+        if n3 == 2 and n4 < 20 :
+            n6 += 1
